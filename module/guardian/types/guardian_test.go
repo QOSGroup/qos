b068da87ce310aaaa28b148f74e78d4dbce42649
imuge <imuge@qq.com>
2019-04-08 20:25:01 +0800

    tests from guardian

---
+++ module/guardian/types/guardian_test.go
@@ -0,0 +1,39 @@
+package types
+
+import (
+	btypes "github.com/QOSGroup/qbase/types"
+	"github.com/stretchr/testify/require"
+	"github.com/tendermint/tendermint/crypto/ed25519"
+	"testing"
+)
+
+func TestGuardian_Equals(t *testing.T) {
+	addr1 := btypes.Address(ed25519.GenPrivKey().PubKey().Address())
+	addr2 := btypes.Address(ed25519.GenPrivKey().PubKey().Address())
+
+	cases := []struct {
+		input1   Guardian
+		input2   Guardian
+		expected bool
+	}{
+		{Guardian{"g1", Genesis, addr1, nil},
+			Guardian{"g1", Genesis, addr1, nil},
+			true},
+		{Guardian{"g1", Genesis, addr1, nil},
+			Guardian{"g2", Genesis, addr1, nil},
+			false},
+		{Guardian{"g1", Genesis, addr1, nil},
+			Guardian{"g1", Ordinary, addr1, nil},
+			false},
+		{Guardian{"g1", Genesis, addr1, nil},
+			Guardian{"g1", Genesis, addr1, addr2},
+			false},
+		{Guardian{"g1", Genesis, addr1, nil},
+			Guardian{"g1", Genesis, addr2, nil},
+			false},
+	}
+
+	for tcIndex, tc := range cases {
+		require.Equal(t, tc.input1.Equals(tc.input2), tc.expected, "tc #%d", tcIndex)
+	}
+}
